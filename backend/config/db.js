538c5d55969835c547cec918502ba2b165e6b188
Amey <amey.powar15@gmail.com>
2025-05-25 19:12:42 +0530

    db changed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const ConnectDB = async ()=>{
-    await mongoose.connect('mongodb://localhost:27017/Hospital-Managment').then(console.log("db connected")
+    await mongoose.connect('mongodb+srv://maxap600:Amey15@cluster0.coc7t.mongodb.net/Hospital-Managment').then(console.log("db connected")
     )
 }
